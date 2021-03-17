b4c3102f8e0d146d0d0c5fec13913322de6457aa
RodrigoSebastian <57414811+RodrigoSebastian@users.noreply.github.com>
2021-03-16 23:21:29 -0600

    PERRO DATABASESCO

---
+++ Node/source/index.js
@@ -0,0 +1,43 @@
+const cuestionarioRoute = require('./routes/cuestionario.route');
+
+// OBLIGATORIO -_-
+
+    //** Requires ***/
+    const express = require('express');
+    const morgan = require('morgan');
+    const cors = require('cors');
+    const path = require('path');
+    const bodyParser = require("body-parser");
+
+    //** Servidor ***/
+    const app = express();
+
+    //Puerto del servidor
+    app.set('port', process.env.PORT || 3000);
+
+    //** Middlewares ***/
+
+    //Log de peticiones por consola
+    app.use(morgan('dev'));
+
+    //Enviar y recibir jsons
+    app.use(bodyParser.json());
+
+    //Aceptar datos de formularios
+    app.use(bodyParser.urlencoded({extended: true}));
+
+
+    //CORS
+    app.use(cors());
+    // Configurar cabeceras y cors
+
+    //** Starting the server ***/
+    app.listen(app.get('port'), () => {
+        console.log('Server on port ', app.get('port'));
+        console.log('Key token: ' + app.get('llavetoken'));
+    });
+
+// OBLIGATORIO -_-
+
+
+app.use('/cuestionario',cuestionarioRoute);
